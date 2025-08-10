6d7ec91445289a3d73cd0914420028a353c29322
Rot4tion <nhonhoem0123@gmail.com>
2025-08-10 20:27:26 +0700

    update logo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
   basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: process.env.PAGES_BASE_PATH + "/",
 };
 
 export default nextConfig;
